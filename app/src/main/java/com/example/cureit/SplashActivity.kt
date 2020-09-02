e0291c3711c2ec6c14ed2a3007d2c07a0926f8b3
sushmoyr <sushmoyr@gmail.com>
2020-09-02 13:47:51 +0600

    Initial commit

---
+++ app/src/main/java/com/example/cureit/SplashActivity.kt
@@ -0,0 +1,20 @@
+package com.example.cureit
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_TIME_OUT:Long = 3000  //3000 in ms
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME_OUT)
+    }
+}
